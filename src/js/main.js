784b895278f7755d7764e5231877b343010ecd93
Ian Holm <ianholm252@gmail.com>
2025-09-18 15:19:08 -0600

    Update 9/18/25

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,30 @@
 import { getParkData } from "./parkService.mjs";
 
 const parkData = getParkData();
+
+const disclaimer = document.querySelector(".disclaimer > a");
+disclaimer.href = parkData.url;
+disclaimer.innerHTML = parkData.fullName;
+
+function parkInfoTemplate(info) {
+  return `<a href="/" class="hero-banner__title">${info.name}</a>
+  <p class="hero-banner__subtitle">
+    <span>${info.designation}</span>
+    <span>${info.states}</span>
+  </p>`;
+}
+
+function setHeroImage(info) {
+  const heroImg = document.getElementById("hero-img");
+  heroImg.src = info.images[0].url;
+  heroImg.alt = info.images[0].altText;
+}
+
+function setPageTitle(info){
+    const title = document.querySelector("title");
+    title.innerText = `${info.name}`;
+}
+
+setPageTitle(parkData);
+setHeroImage(parkData);
+parkInfoTemplate(parkData);
